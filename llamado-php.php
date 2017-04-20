232b337de6e294d1093d68a5325424cdca3f305a
arleycastellanos <arley1132133@gmail.com>
2017-04-19 21:17:30 -0500

    Update llamado-php.php

--- llamado-php.php
+++ llamado-php.php
@@ -1,12 +1,26 @@
 <?php
 
+
+
     include'class/BD.php';
-    $nuevo_obj=new BD();    // llama la clase BD
+    $nb_m=new BD();    // llama la clase BD
            
      if( isset( $_GET[ 'cadena' ] ) )
     {     
         $valores=$_GET['cadena'];
-        echo  $nuevo_obj->consultar($valores);
+        echo  $nb_m->consultar($valores);
         //echo $sql;
+
     }
+     if( isset( $_GET[ 'busqueda' ] ) )//Recibe todo loq ue contiene busqueda y hace una busqueda en la base de datos mediante la función
+    {  
+        if ($_GET['busqueda']!="buscar.php") 
+        {
+           $valores=$_GET['busqueda'];
+           echo  $nb_m->buscar($valores);//Se trae la función buscar que se encuentra en el BD.php
+           echo $sql;
+        }
+        
+    }
+
   ?>
