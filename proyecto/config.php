289a6f1a2c871b5c06361d72487e6564815ded54
arleycastellanos <arley1132133@gmail.com>
2017-04-25 15:40:13 -0500

    Create config.php

---
+++ proyecto/config.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+*autores: jeison salgado -dubier perez
+*/
+	$servidor="localhost";
+	$usuario="root";
+	$clave="";
+	$bd="bd_proyecto";
+	$sn_pruebas="s";
+	$sn_pruebas_log="s";
+
+ ?>
